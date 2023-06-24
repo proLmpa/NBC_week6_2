5a44c43706ba9db214ba06078047085dbbc501ed
heeyeoul kim <kheeyeoul@gmail.com>
2023-06-24 14:52:31 +0900

    JWT Authentication & Authorization filter class 를 추가하여 JWT를 이용한 login 기능 구현

--- src/main/java/com/sparta/post/config/WebSecurityConfig.java
+++ src/main/java/com/sparta/post/config/WebSecurityConfig.java
@@ -1,16 +1,46 @@
 package com.sparta.post.config;
 
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import com.sparta.post.jwt.JwtAuthenticationFilter;
+import com.sparta.post.jwt.JwtAuthorizationFilter;
+import com.sparta.post.jwt.JwtUtil;
+import com.sparta.post.security.UserDetailsServiceImpl;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
 @EnableWebSecurity  // Spring Security 지원을 가능하게 함
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class WebSecurityConfig {
+    private final JwtUtil jwtUtil;
+    private final UserDetailsServiceImpl userDetailsService;
+    private final AuthenticationConfiguration authenticationConfiguration;
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter() throws Exception {
+        JwtAuthenticationFilter filter = new JwtAuthenticationFilter(jwtUtil);
+        filter.setAuthenticationManager(authenticationManager(authenticationConfiguration));
+        return filter;
+    }
+
+    @Bean
+    public JwtAuthorizationFilter jwtAuthorizationFilter() {
+        return new JwtAuthorizationFilter(jwtUtil, userDetailsService);
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         // CSRF 설정
@@ -21,8 +51,13 @@ public class WebSecurityConfig {
                         .requestMatchers("/api/**").permitAll() // '/api/'로 시작하는 요청 모두 접근 허가
                         .anyRequest().authenticated() // 그 외 모든 요청 인증처리
         );
+//
+//        http.formLogin(
+//                (formLogin) -> formLogin.loginPage("/api/user/login-page").permitAll());
 
-        http.formLogin(AbstractAuthenticationFilterConfigurer::permitAll);
+        // 필터 관리
+        http.addFilterBefore(jwtAuthorizationFilter(), JwtAuthenticationFilter.class);
+        http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
