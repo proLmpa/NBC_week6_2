a904911a4015150313ea8dead824d26a259c6822
heeyeoul kim <kheeyeoul@gmail.com>
2023-06-24 14:53:07 +0900

    JWT filter 추가로 인한 코드 간략화

--- src/main/java/com/sparta/post/controller/UserController.java
+++ src/main/java/com/sparta/post/controller/UserController.java
@@ -5,11 +5,17 @@ import com.sparta.post.dto.UserResponseDto;
 import com.sparta.post.service.UserService;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@Slf4j
 @RestController // API 형식으로 반환하는 Controller 형식
 @RequestMapping("/api")
 public class UserController {
@@ -21,20 +27,17 @@ public class UserController {
     }
 
     @PostMapping("/user/signup")
-    public UserResponseDto signup(@Valid @RequestBody UserRequestDto requestDto) {
-        userService.signup(requestDto);
-        return new UserResponseDto(200L, "SIGN_UP_SUCCESS");
-    }
-
-    @PostMapping("/user/login")
-    public UserResponseDto login(@Valid @RequestBody UserRequestDto requestDto, HttpServletResponse res) {
-        try {
-            userService.login(requestDto, res);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return new UserResponseDto(401L, "LOG_IN_FAILURE");
+    public UserResponseDto signup(@Valid @RequestBody UserRequestDto requestDto, BindingResult bindingResult) {
+        // Validation 예외 처리
+        List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+        if(fieldErrors.size() > 0) {
+            for(FieldError fieldError : bindingResult.getFieldErrors()) {
+                log.error(fieldError.getField() + " 필드 : " + fieldError.getDefaultMessage());
+            }
+            return new UserResponseDto(401L, "SIGN_UP_FAILURE");
         }
 
-        return new UserResponseDto(200L, "LOG_IN_SUCCESS");
+        userService.signup(requestDto);
+        return new UserResponseDto(200L, "SIGN_UP_SUCCESS");
     }
 }
