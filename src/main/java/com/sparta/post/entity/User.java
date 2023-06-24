0df8966dc59c7c89bb04d30cac085c2c5a0ed6e8
heeyeoul kim <kheeyeoul@gmail.com>
2023-06-24 13:36:01 +0900

    사용자명, 비밀번호 제한사항을 User class 에서 UserReqeustDto class로 넘겨 User에 저장되기 전부터 유효성 검사를 실행하도록 변경

--- src/main/java/com/sparta/post/entity/User.java
+++ src/main/java/com/sparta/post/entity/User.java
@@ -16,11 +16,9 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Pattern(regexp="^[a-z0-9]{4,10}$", message="영소문자와 숫자(0~9)로 이뤄진 4자 이상 10자 이하의 값으로 이뤄졌습니다.")
     @Column(name = "username", nullable = false)
     private String username;
 
-    @Pattern(regexp="^[a-zA-Z0-9]{8,15}$", message="영대소문자와 숫자(0~9)로 이뤄진 8자 이상 15자 이하의 값으로 이뤄졌습니다.")
     @Column(name = "password", nullable = false)
     private String password;
 
