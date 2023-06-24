57a1b02047c48bed86dc903b36ec9cc183915538
heeyeoul kim <kheeyeoul@gmail.com>
2023-06-24 12:55:24 +0900

    TimeStamped class에 수정 시간 필드 modifiedAt을 추가하고 PostRequestDto에 추가

--- src/main/java/com/sparta/post/dto/PostResponseDto.java
+++ src/main/java/com/sparta/post/dto/PostResponseDto.java
@@ -11,11 +11,13 @@ public class PostResponseDto {
     private String username;
     private String contents;
     private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
 
     public PostResponseDto(Post post) {
         this.title = post.getTitle();
         this.username = post.getUsername();
         this.contents = post.getContents();
         this.createdAt = post.getCreatedAt();
+        this.modifiedAt = post.getModifiedAt();
     }
 }
--- src/main/java/com/sparta/post/entity/TimeStamped.java
+++ src/main/java/com/sparta/post/entity/TimeStamped.java
@@ -4,6 +4,7 @@ package com.sparta.post.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
@@ -16,4 +17,9 @@ public abstract class TimeStamped {
     @Column(updatable = false)
     @Temporal(TemporalType.TIMESTAMP)
     public LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime modifiedAt;
 }
